fe6f395ce28d78e06eae7356288a97812d5d336a
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2022-10-28 14:21:05 -0300

    gets e sets das classe filme

--- src/atv01/objs/Filme.java
+++ src/atv01/objs/Filme.java
@@ -2,4 +2,26 @@ package atv01.objs;
 
 public class Filme {
 	private String titulo;
+	private String diretor;
+	
+	public Filme(String titulo, String diretor) {
+		this.titulo = titulo;
+		this.diretor = diretor;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getTitulo() {
+		return this.titulo;
+	}
+	
+	public void setDiretor(String diretor) {
+		this.diretor = diretor;
+	}
+	
+	public String getDiretor() {
+		return this.diretor;
+	}
 }
