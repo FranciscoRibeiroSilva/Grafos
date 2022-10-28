2c7d56e36ab19c82c510df039cbb808ab7a45a2c
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2022-10-28 15:32:21 -0300

    tocando tipo das matizes e removendo os os atributos index na classe grafo

--- src/atv01/objs/Grafo.java
+++ src/atv01/objs/Grafo.java
@@ -1,58 +1,26 @@
 package atv01.objs;
 
 public class Grafo {
-	private Filme filmes [][];
-	private int indexF;
-	private Ator atores [][];
-	private int indexA;
-	private Object actFilm [][];
-	private int indexAct;
+	private int filmes [][];
+	private int atores [][];
+	private int actFilm [][];
 	
 	private Grafo() {
-		this.actFilm = new Object [11][11];
-		this.indexAct = 0;
-		this.atores = new Ator[5][5];
-		this.indexA = 0;
-		this.filmes = new Filme[5][5];
-		this.indexF = 0;
+		this.actFilm = new int [11][11];
+		this.atores = new int[5][5];
+		this.filmes = new int[5][5];
 	}
 	
-	public void addAtor(Ator ator) {
-		this.atores[this.indexA][this.indexA] = ator;
-		setIndexA();
+	public void setAtor(int indexA, int indexB) {
+		this.atores[indexA][indexB] = 1;
 	}
 	
-	public void addFilme(Filme filme) {
-		this.filmes[this.indexF][this.indexF] = filme;
-		setIndexF();
+	public void setFilme(int indexA, int indexB) {
+		this.filmes[indexA][indexB] = 1;
 	}
 	
-	public void addAtor(Object atc) {
-		this.actFilm[this.indexAct][this.indexAct] = atc;
-		setIndexAct();
+	public void addAtor(int indexA, int indexB) {
+		this.actFilm[indexA][indexB] = 1;
 	}
 	
-	public void setIndexF() {
-		this.indexF++;
-	}
-	
-	public int getIndexF() {
-		return this.indexF;
-	}
-	
-	public void setIndexA() {
-		this.indexA++;
-	}
-	
-	public int getIndexA() {
-		return this.indexA;
-	}
-	
-	public void setIndexAct() {
-		this.indexAct++;
-	}
-	
-	public int getIndexAct() {
-		return this.indexAct;
-	}
 }
