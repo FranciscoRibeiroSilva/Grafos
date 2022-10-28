70b5771d72d9c89d6c90a191bd64383ac3814675
FranciscoRibeiroSilva <franciscoribeirsil@gmail.com>
2022-10-28 14:26:24 -0300

    gets e sets das classes ator e diretor

--- src/atv01/objs/Ator.java
+++ src/atv01/objs/Ator.java
@@ -3,4 +3,16 @@ package atv01.objs;
 public class Ator {
 	private String nome;
 	
+	public Ator (String nome) {
+		this.nome = nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
 }
--- src/atv01/objs/Diretor.java
+++ src/atv01/objs/Diretor.java
@@ -2,5 +2,17 @@ package atv01.objs;
 
 public class Diretor {
 	private String nome;
+	
+	public Diretor(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
 
 }
